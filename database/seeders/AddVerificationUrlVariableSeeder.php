39fd24ca5a2fd9a8e71b2c6527543fc09bbd8700
Vijay <vijay@stelleninfotech.com.au>
2025-12-29 14:42:39 +0530

    fix seeder

--- database/seeders/AddVerificationUrlVariableSeeder.php
+++ database/seeders/AddVerificationUrlVariableSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\TemplateVariable;
 use Illuminate\Database\Seeder;
 
 class AddVerificationUrlVariableSeeder extends Seeder
@@ -12,6 +12,17 @@ class AddVerificationUrlVariableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        TemplateVariable::updateOrCreate(
+            ['name' => 'verification_url'],
+            [
+                'name' => 'verification_url',
+                'module' => 'general',
+                'description' => 'Email verification link',
+                'example_value' => rtrim(config('app.url'), '/').'/signup/verify?id=1&hash=abc123&signature=xyz',
+                'is_active' => true,
+            ]
+        );
+
+        $this->command->info('Verification URL variable seeded successfully!');
     }
 }
